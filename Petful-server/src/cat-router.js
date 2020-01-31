bc14334ff9461e144154b5058a2b79c96b9fcd77
Jack <pupeljack@gmail.com>
2020-01-31 11:06:58 -0600

    made dog and cat routers better

--- Petful-server/src/cat-router.js
+++ Petful-server/src/cat-router.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const PetRouter = express.Router;
+const {cats} = require('./pets');
+let _cat = cats.dequeue();
 
 PetRouter
     .route('/')
     .get((req, res) =>{
-
+        if(_cat === null){
+            res.status(404).send('error: no more cats');
+        } else {
+            res.json(_cat);
+        }
+    })
+    .delete((req, res) =>{
+        _cat = cats.dequeue();
+        return res.status(204).json(_cat);
     })
 
 
--- Petful-server/src/dog-router.js
+++ Petful-server/src/dog-router.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const PetRouter = express.Router;
+const {dogs} = require('./pets');
+
 
 PetRouter
     .route('/')
     .get((req, res) =>{
-
+        if(_dog === null){
+            return res.status(404).send('error: no more dogs');
+        } else {
+            return res.json(dogs.first);
+        }
+    })
+    .delete((req, res) =>{
+        dogs.dequeue();
+        return res.status(204);
     })
 
 
